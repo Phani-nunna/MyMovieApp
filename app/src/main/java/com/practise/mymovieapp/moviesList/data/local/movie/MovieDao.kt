34048f569b4c1d14bb9ce2c24770ca169949af09
Phani-nunna <Phani-nunna@users.noreply.github.com>
2024-03-11 16:35:07 +0530

    Update MovieDao.kt
    add space

--- app/src/main/java/com/practise/mymovieapp/moviesList/data/local/movie/MovieDao.kt
+++ app/src/main/java/com/practise/mymovieapp/moviesList/data/local/movie/MovieDao.kt
@@ -13,6 +13,7 @@ interface MovieDao{
  @Query("SELECT * FROM MovieEntity WHERE id=:id")
  suspend fun getMovieById(id:Int):MovieEntity
 
+ 
     @Query("SELECT * FROM MovieEntity WHERE category=:category")
     suspend fun getMovieListByCategeory(category:String):List<MovieEntity>
 }
